af442993de9d57213a1a36020635d748d811f133
naazneen <jatunazneen@gmail.com>
2020-09-22 07:51:18 +0530

    changes

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.db import models
 from django.utils.timezone import datetime
 from django.contrib.auth.models import User
+from utils import create_new_ref_number
 
 
 # Create your models here.
